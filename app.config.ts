f75d767daa18098bb1e588c347f13a61aaa84c4b
Leonardo Barros <leonprogramer@gmail.com>
2023-12-11 16:43:08 -0300

    Andoid configuration added

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,9 @@ import { ExpoConfig } from 'expo/config';
 const config: ExpoConfig = {
   name: 'masternutri',
   slug: 'masternutri',
+  "android": {
+    "package": "com.masternutri"
+  }
 };
 
 export default config;
